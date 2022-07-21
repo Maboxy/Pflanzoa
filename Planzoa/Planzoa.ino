f6e8b97c2fb17172b26455a7752eb5917127e49f
Paul Hackenberg <47890544+Maboxy@users.noreply.github.com>
2022-07-21 19:47:48 +0200

    Init

---
+++ Planzoa/Planzoa.ino
@@ -0,0 +1,26 @@
+class Pflanze{
+    private:
+      string name;
+      int Feuchtigkeit;
+      int Dauer;
+      
+    public:
+      Pflanze();
+      ~Pflanze()
+      
+    
+     
+  };
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
