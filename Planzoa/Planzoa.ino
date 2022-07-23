719d9e6efbabb1a574c4b388074ce4bd4d17acdb
Paul Hackenberg <47890544+Maboxy@users.noreply.github.com>
2022-07-23 19:01:45 +0200

    Klasse angelegt, Sensor und Pump Eingebunden

--- Planzoa/Planzoa.ino
+++ Planzoa/Planzoa.ino
@@ -1,50 +1,65 @@
-
+#include <ArduinoSTL.h>
 
 using namespace std;
 
-class Pflanze{
-    private:
-      String name;
-      int Feuchtigkeit;
-      int Dauer;
-      
-    public:
-      Pflanze();
-      ~Pflanze();
-      void getDauer();
-      
-    
-     
-  };
+class Pflanze {
+  private:
+    String name;
+    int Feuchtigkeit;
+    int Dauer;
 
- Pflanze::Pflanze(String name_, int Feuchtigkeit_, int Dauer_){
+  public:
+    Pflanze(String name_ = "Test", int Feuchtigkeit_ = 50, int Dauer_ = 1000);
+    ~Pflanze();
+    int getDauer();
+
+
+
+};
+
+Pflanze::Pflanze(String name_, int Feuchtigkeit_, int Dauer_) {
   name = name_;
   Feuchtigkeit = Feuchtigkeit_;
   Dauer = Dauer_;
-  }
+}
 
- void Pflanze::getDauer(){
+int Pflanze::getDauer() {
   return Dauer;
-  }
+}
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  Pflanze* Basili = new Pflanze();
+  Pflanze* Basili = new Pflanze("Basili", 10, 500);
   pinMode(12, OUTPUT);
+  pinMode(A0, INPUT);
+
+  Serial.begin(9600);
+
+  vector<Pflanze*> *Pflanzen = new vector<Pflanze*>;
+
+  Pflanzen->pushBack(new Pflanze("Basili", 1000, 2000));
+
   
 
+  test(Basili);
 }
 
+void test(Pflanze *Basili) {
+
+  while (true) {
+    /*
+      digitalWrite(12, HIGH);
+      delay(Basili->getDauer());
+      digitalWrite(12, LOW);
+      delay(Basili->getDauer()); */
+
+    int Wert = analogRead(A0);
+
+    Serial.println(Wert);
 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(12, HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(1000);
 
-
+  }
 }
