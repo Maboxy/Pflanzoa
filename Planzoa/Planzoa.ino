194f4cf2b98a023ec89f1cf08a5945b68e88fe0c
Paul Hackenberg <47890544+Maboxy@users.noreply.github.com>
2022-07-23 22:05:57 +0200

    Bugfixes

--- Planzoa/Planzoa.ino
+++ Planzoa/Planzoa.ino
@@ -9,25 +9,28 @@ class Pflanze {
     int Feuchtigkeit;
     int Dauer;
     int Pin;
+    int SensorPin;
 
   public:
-    Pflanze(String name_ = "Test", int Feuchtigkeit_ = 50, int Dauer_ = 1000, int Pin_ = 0);
+    Pflanze(String name_ = "Test", int Feuchtigkeit_ = 50, int Dauer_ = 1000, int Pin_ = 0, int SensorPin_ = A0);
     ~Pflanze();
     int getDauer();
     int getFeuchtigkeit();
     int getPin();
+    int getSensorPin();
 
 
 
 };
 
 //Konstruktor
-Pflanze::Pflanze(String name_, int Feuchtigkeit_, int Dauer_, int Pin_) {
+Pflanze::Pflanze(String name_, int Feuchtigkeit_, int Dauer_, int Pin_, int SensorPin_) {
   name = name_;
   Feuchtigkeit = Feuchtigkeit_;
   Dauer = Dauer_;
   Pin = Pin_;
-  }
+  SensorPin = SensorPin_;
+}
 
 int Pflanze::getDauer() {
   return Dauer;
@@ -37,9 +40,14 @@ int Pflanze::getFeuchtigkeit() {
   return Feuchtigkeit;
 }
 
-int Pflanze::getPin(){
+int Pflanze::getPin() {
   return Pin;
+}
+
+int Pflanze::getSensorPin(){
+  return SensorPin;
   }
+
 
 
 void setup() {
@@ -51,37 +59,49 @@ void setup() {
 
   vector<Pflanze*> *Pflanzen = new vector<Pflanze*>;
 
-  Pflanzen->push_back(new Pflanze("Basili", 1000, 3000, 12));
-  Pflanzen->push_back(new Pflanze("Boni", 1000, 2000, 13));
+  Pflanzen->push_back(new Pflanze("Basili", 500, 3000, 12, A0));
+  Pflanzen->push_back(new Pflanze("Boni", 500, 2000, 13, A1));
+  Pflanzen->push_back(new Pflanze("Minzi", 500, 2000, 8, A2));
+  Pflanzen->push_back(new Pflanze("Risi", 500, 2000, 7, A3));
   digitalWrite(12, HIGH);
 
 
-  test(Pflanzen);
+  RunScript(Pflanzen);
 }
 
-void test(vector<Pflanze*> *Pflanzen) {
+//Funktion enthaehlt den eigentlichen Programablauf
+void RunScript(vector<Pflanze*> *Pflanzen) {
 
+  //Endlosschleife bis zum Reset des Controllers
   while (true) {
-    /*
-      digitalWrite(12, HIGH);
-      delay(Basili->getDauer());
-      digitalWrite(12, LOW);
-      delay(Basili->getDauer()); */
-
-    int Wert = analogRead(A0);
-
-    Serial.println(Wert);
-
-
-
+    //Schleife geht alle Pflanzen durch
     for (int i = 0; i < Pflanzen->size(); i++) {
+      //Feuchte Wert des Sensors zur Pflanze
+      int Wert = analogRead(Pflanzen->at(i)->getSensorPin());
 
-      if (Wert > Pflanzen->at(i)->getFeuchtigkeit() ) {
+      //Ausgabe des Wertes an den SerialMonitor
+      Serial.println(Wert);
+
+      //Mittelwert aus 5 Werten ermittlen
+      for(int a = 0; a < 4; a++){
+        Wert += analogRead(Pflanzen->at(i)->getSensorPin());
+        }
+
+        Wert = Wert/ 5;
+
+        Serial.println("Mittelwert= "+ Wert);
+       
+
+      //Fuer jede Pflanze die Feuchtigkeit ueberpruefen und ggf. Waessern ´
+      if (analogRead(Wert) > Pflanzen->at(i)->getFeuchtigkeit() ) {
+        Serial.println(Wert);
         digitalWrite(Pflanzen->at(i)->getPin(), LOW);
         delay(Pflanzen->at(i)->getDauer());
         digitalWrite(Pflanzen->at(i)->getPin(), HIGH);
       }
+
       
+
     }
 
 
