1a04ae7235f46552b6d5a4fcff82af3b4a7e2776
Paul Hackenberg <47890544+Maboxy@users.noreply.github.com>
2022-07-23 17:56:55 +0200

    ERste versuche

--- Planzoa/Planzoa.ino
+++ Planzoa/Planzoa.ino
@@ -1,20 +1,39 @@
+
+
+using namespace std;
+
 class Pflanze{
     private:
-      string name;
+      String name;
       int Feuchtigkeit;
       int Dauer;
       
     public:
       Pflanze();
-      ~Pflanze()
+      ~Pflanze();
+      void getDauer();
       
     
      
   };
 
+ Pflanze::Pflanze(String name_, int Feuchtigkeit_, int Dauer_){
+  name = name_;
+  Feuchtigkeit = Feuchtigkeit_;
+  Dauer = Dauer_;
+  }
+
+ void Pflanze::getDauer(){
+  return Dauer;
+  }
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  Pflanze* Basili = new Pflanze();
+  pinMode(12, OUTPUT);
+  
 
 }
 
@@ -22,5 +41,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+
 
 }
